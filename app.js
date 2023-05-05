029fa3dac9b0b1c8b821fc72d851abb92095004b
Victoria <vvictoriaandres@gmail.com>
2023-05-05 15:05:24 -0300

    Definimos la ruta de un curso con parametro url

--- app.js
+++ app.js
@@ -12,8 +12,31 @@ app.get("/", (req, res) => {
 });
 
 // Definimos la ruta de cursos
-app.get("/cursos", (req, res) => {
-  res.send(infoCursos);
+app.get("/api/cursos", (req, res) => {
+  res.send(JSON.stringify(infoCursos));
+});
+
+// Definimos la ruta de un curso
+app.get("/api/cursos/programacion", (req, res) => {
+  res.send(JSON.stringify(infoCursos.programacion));
+});
+
+app.get("/api/cursos/matematicas", (req, res) => {
+  res.send(JSON.stringify(infoCursos.matematicas));
+});
+
+// Definimos la ruta de un curso con parametro url
+app.get("/api/cursos/:lenguaje", (req, res) => {
+  const lenguaje = req.params.lenguaje;
+  const resultado = infoCursos.programacion.filter((curso) => {
+    return curso.lenguaje === lenguaje;
+  });
+
+  if (resultado.length === 0) {
+    return res.status(404).send(`"El curso ${lenguaje} no existe"`);
+  }
+
+  res.send(JSON.stringify(resultado));
 });
 
 // Definimos el puerto
